bf605759953005505b89e9b3fde3d18dee7fed67
Juan Medina <juanmedina@seti.com.co>
2024-09-19 10:31:36 -0500

    prompt sql agent

--- src/QADBSQLAgent/chain.py
+++ src/QADBSQLAgent/chain.py
@@ -26,17 +26,20 @@ db =SQLDatabase.from_uri("sqlite:///data/sqlite/cierre.db")
 
 
 prefix = """
-
 La tabla con la que interactuas es cierre. La cual contiene registros de las tareas realizadas para el proceso de cierre
 La FECHA_CIERRE indica la fecha del día sobre el que se ejecuta el proceso de cierre. TODAS las tareas con la misma FECHA_CIERRE son parte del mismo cierre
 Cada tarea está identificada por el CODIGO_TAREA y DESCRIPCION_TAREA. 
 Cada tarea tiene INICIO y FIN que contienen la fecha y hora de inicio y fin de la ejecución
-DURACION es la duración de le ejecución en segundos
-El proceso inicia con la tarea identificada por HPBDESC='Inhabilita accesos al menu', 
+La columna DURACION tiene el total de segundos de ejecución, se puede sumar para saber el tiempo total
+La preguntas de duración respndela en horas, minutos y segundos.
+El proceso inicia con la tarea identificada por DESCRIPCION_TAREA='Inhabilita accesos al menu', 
 el INICIO de esta tarea es inicio del proceso de cierre.
 El fin del proceso de cierre se da al finalizar la tarea con CODIGO_TAREA=OCIE1000.
-Si esta proceso Finaliza despues de la 08:00:00 es un afectación que no permite abrir oficinas.
-La respuesta debe ser completa pero ejecutiva para que la reciban los altos ejecutivos de la compañía
+Si la tarea identificadas con DESCRIPCION_TAREA='Habilita accesos al menu'
+finaliza después de las 08:00:00 del dia siguiente a la fecha de cierre no se pueden abrir oficinas a tiempo
+Para saber la duracion de todo el proceso suma los segundos de todas las tareas para la FECHA_CIERRE
+Para saber la duracion sin pausas suma todas las tareas con CODIGO_TAREA != PAUSA
+La respuesta debe ser completa pero ejecutiva en español para que la reciban los altos ejecutivos de la compañía
 """
 
 
@@ -77,8 +80,26 @@ def main():
 
         inputs = {"messages": [("user", user_input)]}
 
-        response = agent_executor.invoke(inputs)
-        response[-1].pretty_print()
+        response = agent_executor.stream(inputs)
+        for s in response:
+            for key in s.keys():
+                message=s[key]['messages'][-1]
+                if isinstance(message,tuple):
+                    print(message)
+                else:
+                    message.pretty_print()
+           
+
+
+"""
+        if isinstance(message, str):
+            print(message)
+        elif isinstance(message, ChatMessage):
+            message.pretty_print()
+        else:
+            print(f"ERROR: Unknown type - {type(message)}")
+"""
+
 
 
 if __name__ == "__main__":
