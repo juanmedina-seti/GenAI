e0f995bccb33e9e82642a1d3738ee55d917899ac
Juan Medina <juanmedina@seti.com.co>
2024-09-25 14:41:57 -0500

    ajustes cierre cali, lectura bdd

--- src/QACierreCaliMem/app.py
+++ src/QACierreCaliMem/app.py
@@ -10,7 +10,7 @@ if 'chat_memory' not in st.session_state:
     st.session_state.chat_memory = ChatMessageHistory()
 
 # Configuración de la aplicación
-st.title("Chatbot con LangChain y Streamlit")
+st.title("Consulta Cierre 10 últimos días")
 
 # Mostrar el historial de la conversación
 def display_chat(history: ChatMessageHistory):
@@ -32,14 +32,19 @@ if prompt := st.chat_input("Pregunta"):
     if prompt:
         # Obtener respuesta del modelo
         st.session_state.chat_memory.add_user_message(prompt)
+        with st.chat_message("user"):
+            st.markdown(prompt)
+                
         response = get_response(prompt)
         st.session_state.chat_memory.add_ai_message(response)
         # Mostrar la respuesta
+
+        with st.status("Consultando información ..." , expanded=True) as status:
+            with st.chat_message("assistant"):
+                st.markdown(response) 
+            status.update(label="",state="complete")      
+
         
-        with st.chat_message("user"):
-            st.markdown(prompt)
-        with st.chat_message("assistant"):
-            st.markdown(response)       
-        # Limpiar el campo de entrada
+            # Limpiar el campo de entrada
 
 
